5934e9d187a8d1cd7e0e87b2bbd2bd9a46525dff
Wesllei <Garcia@macbook-pro-de-wesllei.local>
2015-08-07 10:38:24 -0400

    Criação das Principais Telas e suas Classes

---
+++ ARegraEClara/ARegraEClara/UIViewControllerFimJogo.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewControllerFimJogo.swift
+//  ARegraEClara
+//
+//  Created by Wesllei on 07/08/15.
+//  Copyright © 2015 Wesllei. All rights reserved.
+//
+
+import UIKit
+
+class UIViewControllerFimJogo: UIViewController {
+
+}
---
+++ ARegraEClara/ARegraEClara/UIViewControllerTopicosRegras.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewControllerTopicosRegras.swift
+//  ARegraEClara
+//
+//  Created by Wesllei on 07/08/15.
+//  Copyright © 2015 Wesllei. All rights reserved.
+//
+
+import UIKit
+
+
+class UIViewControllerTopicosRegras: UIViewController {
+
+}
